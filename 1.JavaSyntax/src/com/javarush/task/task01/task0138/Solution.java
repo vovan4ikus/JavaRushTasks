1de3948d85d42516a3961d2083647c01d428e56a
Ustimenko Vladimir <ustimmmenko@gmail.com>
2017-02-18 15:03:28 +0200

    done

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("kzkzkzk kefjnf");
+        System.out.println("hgbrgkjnosiegh oignrg n");
+        System.out.println("true la-la");
+        System.out.println("ifjoigwroighergiuh riogjhoerigjoeirg oijrgoiherog");
+        //напишите тут ваш код
+    }
+}
