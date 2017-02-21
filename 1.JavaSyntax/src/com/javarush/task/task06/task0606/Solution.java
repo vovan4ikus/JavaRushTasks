ac83040fdb1f5d619c362422595863f188980589
paramon4ik <ustimmmenko@gmail.com>
2017-02-21 21:11:06 +0200

    done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -13,5 +13,16 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        char a[] = reader.readLine().toCharArray();
+        reader.close();
+        int b[] = new int[a.length];
+        for (int i = 0; i < a.length; i++){
+            b[i] = Integer.valueOf(a[i]);
+            if (b[i] % 2 == 0) even++;
+            else odd ++;
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
+        //напишите тут ваш код
     }
 }
